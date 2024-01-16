420785cdf98d1cd636daa6cd02365b0007aeef4c
tjqwn <tjqwn@ahn-laptop>
2024-01-16 12:01:51 +0900

    commit

---
+++ src/study0116/Arr5.java
@@ -0,0 +1,35 @@
+package study0116;
+
+
+public class Arr5 {
+
+	public static void main(String[] args) {
+		String[] arrstr = {new String("one"),
+						new String("two"),
+						new String("three")
+						};
+		int[] ar = {1,2,3,4,5,6,7,};
+		int sum=sumOfAry(ar);
+		int[] arrInt1 = new int[3];
+		arrInt1[0] = 1;
+		arrInt1[1] = 2;
+		arrInt1[2] = 3;
+		
+		System.out.println(arrInt1[0]);
+		System.out.println(arrInt1[1]);
+		System.out.println(arrInt1[2]);
+		System.out.println(arrstr[0]);
+		System.out.println(arrstr[1]);
+		System.out.println(arrstr[2]);
+		}
+	static int sumOfAry(int[] ar) {
+		int sum = 0;
+		for(int i = 0; i< ar.length;i++) {
+			sum+=ar[i];
+		}
+		System.out.println(sum);
+		return sum;
+	}
+	}
+
+
--- src/study0116/Arr6.java
+++ src/study0116/Arr6.java
@@ -2,30 +2,40 @@ package study0116;
 
 import java.util.Scanner;
 
-public class Arr3 {
+public class Arr6 {
+	public static double sumOfCircle(Circle3[] arrCircle) {
+		double sum = 0;
+		for(int i =0;i < arrCircle.length;i++) {
+			sum += arrCircle[i].getArea();
+		}
+		System.out.println(sum);
+		return sum;
+	}
 
 	public static void main(String[] args) {
 		final int CIRCLE_MAX = 3;
-		Circle[] car = new Circle[CIRCLE_MAX];
+		Circle3[] arrCircle = {new Circle3(5),new Circle3(10),new Circle3(100)};
+		Circle3[] car = new Circle3[CIRCLE_MAX];
 		Scanner sc = new Scanner(System.in);
+		double sircleSum = sumOfCircle(arrCircle);
 
 		double sum = 0;
 		for(int i =0; i< car.length;i++) {
 			System.out.println("반지름을 입력하세요");
 			System.out.print(">>");
 			int ra = sc.nextInt();
-			car[i] = new Circle(ra);
+			car[i] = new Circle3(ra);
 			sum+=car[i].getArea();
 		}
 		System.out.println("서클 넓이의 합 : " + sum);
 	}
 }
 
-class Circle {
+class Circle3 {
 	static final double PI = 3.1415;
 	private double radius;
 
-	Circle(double rad) {
+	Circle3(double rad) {
 		radius = rad;
 	}
 	double getArea() {
