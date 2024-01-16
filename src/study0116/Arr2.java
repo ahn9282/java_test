65d1de8b997ad9cf480bea4d4605fab48909a345
tjqwn <tjqwn@ahn-laptop>
2024-01-16 10:13:59 +0900

    commit

---
+++ src/study0116/Arr2.java
@@ -0,0 +1,29 @@
+package study0116;
+
+public class Arr2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box[] ar = new Box[3];
+		ar[0] = new Box("First");
+		ar[1] = new Box("Second");
+		ar[2] = new Box("Third");
+
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+	}}
+
+	class Box {
+		private String conts;
+
+		Box(String conts) {
+			this.conts = conts;
+		}
+
+		public String toString() {
+			return conts;
+		}
+	}
+
+
